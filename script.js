81a684fb0ff36920ba926ea5291b7da8d3284392
Narek20029 <bnarek.50@gmail.com>
2018-11-28 17:19:52 +0400

    yntira

--- script.js
+++ script.js
@@ -25,23 +25,36 @@ var side = 20;
 var grassArr = [], xotakerArr = [], gishatichArr = [], miteorArr = [], dinoArr = [], patArr = [], jurArr = [], stexcoxArr = [];
 var boomX;
 var boomY;
-var xotakerP;
-var gishatichP;
-var dinozavP;
-var exanakP;
-
+var stats = {
+    xot: 0,
+    xotakerP: 0,
+    gishatichP: 0,
+    dinozavP: 0,
+    exanakP: 0,
+    fps: 0
+}
 var dmer;
 
+socket=io();
+console.log(io());
+
 function setup() {
-    
-    xotakerP = document.getElementById("xotaker");
-    gishatichP = document.getElementById("gishatich");
-    dinozavrP = document.getElementById("dinozavr");
-    exanakP = document.getElementById("exanak");
+    if (Dmer) {
+        dmer = "dmer";
+    }
+    else {
+        dmer = "garun";
+    }
+    stats.xot = grassArr.length;
+    stats.fps = frame;
+    stats.xotakerP = xotakerArr.length;
+    stats.gishatichP = gishatichArr.length;
+    stats.exanakP = dmer;
+    stats.dinozavP = dinoArr.length;
 
 
     stexcoxArr.push(new stexcox(10));
-    frameboom = frame + Math.round(Math.random()* 10);
+    frameboom = frame + Math.round(Math.random() * 10);
     new stexcox(10);
     document.body.style.backgroundColor = "green";
     matrix = genMatrix(w, h);
@@ -54,13 +67,13 @@ function setup() {
                 grassArr.push(new Grass(x * 1, y * 1, 1));
             }
             else if (matrix[y][x] == 2) {
-                xotakerArr.push(new Xotaker(x * 1, y * 1, 2,Math.round(Math.random()*1)));
+                xotakerArr.push(new Xotaker(x * 1, y * 1, 2, Math.round(Math.random() * 1)));
             }
             else if (matrix[y][x] == 3) {
-                gishatichArr.push(new Gishatich(x * 1, y * 1, 3,Math.round(Math.random()*1)))
+                gishatichArr.push(new Gishatich(x * 1, y * 1, 3, Math.round(Math.random() * 1)))
             }
             else if (matrix[y][x] == 4) {
-                dinoArr.push(new dinozavr(x * 1, y * 1, 4,Math.round(Math.random()*1)))
+                dinoArr.push(new dinozavr(x * 1, y * 1, 4, Math.round(Math.random() * 1)))
             }
             else if (matrix[y][x] == 6) {
                 jurArr.push(new jur(x * 1, y * 1, 6))
@@ -75,40 +88,46 @@ function setup() {
 }
 
 function draw() {
+    if(frame%61==0){
+        socket.emit('handleStats',stats);
+        console.log("sended data");
+    }
+    stats.xot = grassArr.length;
+    stats.fps = frame;
+    stats.xotakerP = xotakerArr.length;
+    stats.gishatichP = gishatichArr.length;
+    stats.exanakP = dmer;
+    stats.dinozavP = dinoArr.length;
+
     umStexcel = Math.round(Math.random() * 4);
-            
+
     makeX = Math.round(Math.random() * h);
     makeY = Math.round(Math.random() * w);
     frame += 1;
     boomY = Math.round(Math.random() * h);
     boomX = Math.round(Math.random() * w);
-    
+
     if (frameboom < frame) {
         for (var y in matrix) {
             for (var x in matrix[y]) {
                 if (boomY == y) {
                     if (boomX == x) {
-                      
-                         miteorArr.push(new miteor(x * 1, y * 1, 8));
-                       
-                        
-                         frameboom = frame + Math.round(Math.random()* 10);
+
+                        miteorArr.push(new miteor(x * 1, y * 1, 8));
+
+
+                        frameboom = frame + Math.round(Math.random() * 10);
                     }
                 }
             }
         }
     }
-    if(Dmer){
+    if (Dmer) {
         dmer = "dmer";
     }
-    else{
+    else {
         dmer = "garun";
     }
-
-        xotakerP.innerText="xotakerneri qanaky " + xotakerArr.length;
-        gishatichP.innerText="gishatichneri qanaky " + gishatichArr.length;
-        dinozavrP.innerText="dinozavrneri qanaky " + dinoArr.length;
-        exanakP.innerText="exanak " + dmer;
 
 
     background("#acacac");
@@ -182,11 +201,11 @@ function draw() {
         dinoArr[i].utel();
         dinoArr[i].mahanal();
     }
-    for(var i in miteorArr){
+    for (var i in miteorArr) {
         miteorArr[i].boom();
 
     }
-    for(var i in stexcoxArr){
+    for (var i in stexcoxArr) {
         stexcoxArr[i].stexcel();
 
     }
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
 
-var PORT = process.env.port || 3000
+var stats=fs.readFileSync('stats.json');
 
-app.use('/',express.static('.'));
+app.use(express.static("."));
+app.get('/', function (req, res) {
+   res.redirect('index.html');
+});
 
-app.listen(PORT,()=>{console.log("connected on port " + PORT)})
+io.on('connection', function (socket) {
+    socket.on("handleStats", function (data) {
+        stats=data;
+        fs.writeFileSync('stats.json',JSON.stringify(stats));
+    });
+ });
+ 
+ app.get('/statistics',function(req,res){
+     if(stats.fps){
+     res.send('<h1> fps -'+stats.fps+'</h1>  <h2> xotaker -'+stats.xotakerP+'</h2> <h2> gishatich -'+stats.gishatichP+'</h2> <h2> dinozavr -'+stats.dinozavP+'</h2> <h2> exanak -'+stats.exanakP+'</h2> <h2> xot -'+stats.xot+'</h2>');
+     }else{
+         res.send('');
+     }
+     console.log(stats);
+ })
+
+server.listen(3000);
